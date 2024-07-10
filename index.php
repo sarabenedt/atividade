99030b8aac5ab3cff0542ebdcde3ffee26b58ce6
sarabenedt <sarabenedt@gmail.com>
2024-07-10 16:57:06 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+// atv1
+
+echo "<b> Atividade </b>";
+echo "<br>";
+
+//atv2
+
+$nome = 'Sara';
+$idade = '17';
+$lugar = 'Raposa';
+$curso = 'de programação no senac';
+
+echo "Meu nome é  " . $nome . "<br>";
+echo "Minha idade  " . $idade . "<br>";
+echo "minha cidade é " . $lugar . "<br>";
+echo "faço curso " . $curso . "<br>";
+
+//atv3
+
+$numero1 = (10);
+$numero2 = (2);
+
+$soma = $numero1 + $numero2;
+$subtracao = $numero1 - $numero2;
+$multiplicacao = $numero1 * $numero2;
+$divisao = $numero1 / $numero2;
+
+echo "Soma: " . $soma . "<br>";
+echo "Subtração: " . $subtracao . "<br>";
+echo "Multiplicação: " . $multiplicacao . "<br>";
+if ($numero2 != 0) {
+    echo "Divisão: " . $divisao . "<br>";
+} else {
+    echo "Divisão: Não é possível dividir por zero\n";
+}
+ echo "<br>";
+
+//atv4
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
